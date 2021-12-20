8e3d35f2c2bd3e40b70fc21f3a45ae9973667c00
Simon Lissack <lissack.simon@gmail.com>
2021-12-20 23:36:31 +0000

    Changed wording of properties

--- WebClient/Models/PingModel.cs
+++ WebClient/Models/PingModel.cs
@@ -5,7 +5,7 @@ public class PingModel
     public Guid Id { get; set; }
     public int DelayInSeconds { get; set; }
     public DateTime RequestedAt { get; set; }
-    public DateTime? ReceivedByWorkerAt { get; set; }
-    public DateTime? ProcessedByWorkerAt { get; set; }
+    public DateTime? AcceptedByWorkerAt { get; set; }
+    public DateTime? CompletedByWorkerAt { get; set; }
     public DateTime? CompletedAt { get; set; }
 }
