b62acae0b8331eebb605569ba0664b563ce82217
Simon Lissack <lissack.simon@gmail.com>
2021-12-20 12:23:47 +0000

    Initial web app configuration

---
+++ WebClient/Program.cs
@@ -0,0 +1,43 @@
+using Infrastructure.Messaging.RabbitMq;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true)
+    .AddEnvironmentVariables();
+
+var rabbitMqConfig = new RabbitMqConfiguration();
+builder.Configuration.GetSection(nameof(RabbitMqConfiguration)).Bind(rabbitMqConfig);
+
+builder.Services
+    .InstallRabbitMqInfrastructure(builder.Configuration)
+    .AddSingleton(rabbitMqConfig);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
