1de174ab995cd2ae4bd5a682902db2ac2a9c18a6
Simon Lissack <lissack.simon@gmail.com>
2021-12-21 18:42:16 +0000

    Update silly mistakes

--- Infrastructure.Messaging.RabbitMq/ChannelExtensions.cs
+++ Infrastructure.Messaging.RabbitMq/ChannelExtensions.cs
@@ -8,7 +8,7 @@ public static class ChannelExtensions
     public static IBasicProperties CreateJsonBasicProperties<TMessageType>(this IModel channel)
     {
         var properties = channel.CreateBasicProperties();
-        properties.Type = nameof(TMessageType);
+        properties.Type = typeof(TMessageType).Name;
         properties.ContentType = MediaTypeNames.Application.Json;
 
         return properties;
@@ -16,7 +16,7 @@ public static class ChannelExtensions
 
     public static void QueueBind(this IModel channel, string queueName, string exchangeName, params string[] routingKeys)
     {
-        foreach (var workRequestRoutingKey in RoutingKeys.WorkRequestRoutingKeys)
+        foreach (var workRequestRoutingKey in routingKeys)
         {
             IModelExensions.QueueBind(channel, queueName, exchangeName, workRequestRoutingKey);
         }
