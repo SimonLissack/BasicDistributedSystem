afd3958f045d8396ae9757bbfc6c9b3f6ee09ce0
Simon Lissack <lissack.simon@gmail.com>
2022-12-31 13:36:40 +0000

    Use const

--- src/Infrastructure.Telemetry/TelemetryConstants.cs
+++ src/Infrastructure.Telemetry/TelemetryConstants.cs
@@ -4,6 +4,6 @@ namespace Infrastructure.Telemetry;
 
 public static class TelemetryConstants
 {
-    public static string AppSource = "BasicDistributedSystem";
+    public const string AppSource = "BasicDistributedSystem";
     public static readonly ActivitySource ActivitySource = new(AppSource);
 }
