1539fbdcbf614c1efe0e9cd017b8f217aa2c6cca
Simon Lissack <lissack.simon@gmail.com>
2022-12-31 13:36:31 +0000

    Add tracing to serialization

--- src/Domain.Shared/SerializationExtensions.cs
+++ src/Domain.Shared/SerializationExtensions.cs
@@ -1,11 +1,21 @@
 using System.Text;
 using System.Text.Json;
+using Infrastructure.Telemetry;
 
 namespace Domain.Shared;
 
 public static class SerializationExtensions
 {
-    public static T DeserializeMessage<T>(this byte[] body) => JsonSerializer.Deserialize<T>(body)!;
+    public static T DeserializeMessage<T>(this byte[] body)
+    {
+        using var activity = TelemetryConstants.ActivitySource.StartActivity();
+        return JsonSerializer.Deserialize<T>(body)!;
+    }
 
-    public static byte[] SerializeToMessage<T>(this T message) => Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+    public static byte[] SerializeToMessage<T>(this T message)
+    {
+        using var activity = TelemetryConstants.ActivitySource.StartActivity();
+
+        return Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+    }
 }
